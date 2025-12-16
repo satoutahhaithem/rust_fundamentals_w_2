2c7f8f0d97b9a5b3ebbcb95d24a706de8ad3e9fe
haithem satoutah <satoutahhaithem@gmail.com>
2025-12-16 16:27:03 +0100

    loops in rust

---
+++ loop_in_rust/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut x = 1;
+
+    // continue looping until x > 5
+    loop {
+        println!("x is {}", x);
+
+        x += 1;
+
+        if x > 5 {
+            break;
+        }
+    }
+}
